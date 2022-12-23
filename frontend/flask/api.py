37db7fc161114757889d6c00422b03b6245abf44
Natalie Umanzor <umanzor2949@gmail.com>
2022-12-23 15:38:52 -0500

    Loaded model from s3, installed tf and boto3

--- frontend/flask/api.py
+++ frontend/flask/api.py
@@ -3,20 +3,22 @@ import pandas as pd
 import pickle
 import json
 import boto3
+import tensorflow as tf
 
 app = Flask(__name__)
 
 
 def load_model():
     s3 = boto3.client('s3',
-                    aws_access_key_id='AWS_KEY',
-                    aws_secret_access_key='AWS_SECRET_ACCESS_KEY'
+                    aws_access_key_id='AWS',
+                    aws_secret_access_key='AWS SECRET KEY'
                     )
     with open('saved_model.pb', 'wb') as mod:
         s3.download_fileobj('dsml-alpha-2-covid-radiography-classification', 'data/fullmodel/saved_model.pb', mod)
-        pipe = pickle.load(mod)
-    return pipe
-pipe = load_model()
+        new_model = tf.keras.models.load_model(mod)
+    return new_model
+
+load_model()
 
 @app.route('/', methods = ['GET'])
 def view():
@@ -24,8 +26,9 @@ def view():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    # data    = request.json
-    # return 'This is our model result!'
+    new_model = load_model()
+    data    = request.json
+    new_model.predict(data)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
